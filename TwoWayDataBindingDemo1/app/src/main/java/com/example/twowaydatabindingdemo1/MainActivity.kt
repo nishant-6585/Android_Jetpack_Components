5e0f2b56c8b98f5549666c1e8efb621a6c3d77cc
NkumarZensar <n.kumar3@zensar.com>
2023-11-01 15:59:15 +0530

    Two Way Data Binding Demo 1

---
+++ TwoWayDataBindingDemo1/app/src/main/java/com/example/twowaydatabindingdemo1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.twowaydatabindingdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.twowaydatabindingdemo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewModelFactory: MainActivityViewModelFactory
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModelFactory = MainActivityViewModelFactory(125)
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainActivityViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+    }
+}
